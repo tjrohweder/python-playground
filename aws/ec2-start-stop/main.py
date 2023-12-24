56680c517d403089ff6255bae0469a0081236798
tjrohweder <tjrohweder@gmail.com>
2023-12-24 16:18:41 -0300

    Improved error handling aidn logging capabilities

--- aws/ec2-start-stop/main.py
+++ aws/ec2-start-stop/main.py
@@ -1,34 +1,43 @@
 import boto3
+import logging
 import sys
 
-ec2 = boto3.client('ec2')
-
 def get_running_instances():
-    running_instances = ec2.describe_instances()
+    try:
+        running_instances = ec2.describe_instances()
 
-    instances_ids = []
+        instances_ids = []
 
-    for reservation in running_instances['Reservations']:
-        for instance in reservation['Instances']:
-            instances_ids.append(instance['InstanceId'])
+        for reservation in running_instances['Reservations']:
+            for instance in reservation['Instances']:
+                instances_ids.append(instance['InstanceId'])
 
-    return instances_ids
+        return instances_ids
+
+    except Exception as e:
+        logging.error(f"Error fetching instances: {e}")
+        return []
 
 def ec2_action(instances_ids):
-    action = sys.argv[1]
+    try:
+        action = sys.argv[1]
 
-    if action == 'start':
-        print('Starting instances: ' + str(instances_ids))
-        ec2.start_instances(InstanceIds=instances_ids)
-    elif action == 'stop':
-        print('Stopping instances: ' + str(instances_ids))
-        ec2.stop_instances(InstanceIds=instances_ids)
-    elif action == 'terminate':
-        print('Terminating instances: ' + str(instances_ids))
-        ec2.terminate_instances(InstanceIds=instances_ids)
-    else:
-        print('Invalid action: ' + action)
+        if action == 'start':
+            logging.info('Starting instances: ' + str(instances_ids))
+            ec2.start_instances(InstanceIds=instances_ids)
+        elif action == 'stop':
+            logging.info('Stopping instances: ' + str(instances_ids))
+            ec2.stop_instances(InstanceIds=instances_ids)
+        elif action == 'terminate':
+            logging.info('Terminating instances: ' + str(instances_ids))
+            ec2.terminate_instances(InstanceIds=instances_ids)
+        else:
+            logging.info('Invalid action: ' + action)
+    except Exception as e:
+        logging.error(f"Unable to perform action {e}")
 
 if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    ec2 = boto3.client('ec2')
     instances_ids = get_running_instances()
     ec2_action(instances_ids)
