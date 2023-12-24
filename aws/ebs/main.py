b2e45f2ace4a9ca8bbb93a62b642351e14721b62
tjrohweder <tjrohweder@gmail.com>
2023-12-24 15:38:47 -0300

    Improved error handling aidn logging capabilities

--- aws/ebs/main.py
+++ aws/ebs/main.py
@@ -1,40 +1,50 @@
 import boto3
 import datetime
+import logging
 import pytz
 
-client = boto3.client('ec2')
 interval = 30
 
 def get_ebs():
     now = datetime.datetime.now(pytz.utc)
     start_time = now - datetime.timedelta(days=interval)
+    try:
+        ebs = client.describe_volumes(
+            Filters=[
+                {
+                    'Name': 'status',
+                    'Values': [
+                        'available',
+                    ]
+                }
+            ]
+        )
 
-    ebs = client.describe_volumes(
-        Filters=[
-            {
-                'Name': 'status',
-                'Values': [
-                    'available',
-                ]
-            }
-        ]
-    )
+        ebs_ids = []
 
-    ebs_ids = []
+        for volume in ebs['Volumes']:
+            if volume['CreateTime'].replace(tzinfo=pytz.utc) < start_time:
+                ebs_ids.append(volume['VolumeId'])
 
-    for volume in ebs['Volumes']:
-        if volume['CreateTime'].replace(tzinfo=pytz.utc) < start_time:
-            ebs_ids.append(volume['VolumeId'])
+        return ebs_ids
 
-    return ebs_ids
+    except Exception as e:
+        logging.error(f"Error fething volumes: {e}")
+        return []
 
 def delete_volumes(ebs_ids):
-    for volume in ebs_ids:
-        print('Deleting volumes: ' + str(volume))
-        client.delete_volume(VolumeId=volume)
-    if not ebs_ids:
-        print('No volumes to delete')
+    try:
+        for volume in ebs_ids:
+            logging.info('Deleted volumes: ' + str(volume))
+            client.delete_volume(VolumeId=volume)
+        if not ebs_ids:
+            logging.info('No volumes to delete')
+    except Exception as e:
+        logging.error(f"Error deleting volumes: {e}")
+        return []            
 
 if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    client = boto3.client('ec2')
     ebs_ids = get_ebs()
     delete_volumes(ebs_ids)
