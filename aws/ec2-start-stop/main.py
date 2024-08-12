82cd500367cdfe234d8a9e22fbcdb7408c0f1f69
tjrohweder <tjrohweder@gmail.com>
2024-08-11 23:23:59 -0300

    Improving project automation structure and adding a instance id list option

--- aws/ec2-start-stop/main.py
+++ aws/ec2-start-stop/main.py
@@ -21,29 +21,34 @@ def get_instances(client):
     except Exception as e:
         logging.error(f"Error fetching instances: {e}")
 
-
 def ec2_action(client, instances_ids):
     try:
         action = sys.argv[1]
-        confirmation = input('Are you sure you want to ' + action + ' these instances? [y/n]: \n' + str(instances_ids))
+        if action == 'list':
+            logging.info('Instances available: ' + str(instances_ids))
 
-        if confirmation == 'y':
-            if action == 'start':
-                logging.info('Starting instances: ' + str(instances_ids))
-                client.start_instances(InstanceIds=instances_ids)
-            elif action == 'stop':
-                logging.info('Stopping instances: ' + str(instances_ids))
-                client.stop_instances(InstanceIds=instances_ids)
-            elif action == 'terminate':
-                logging.info('Terminating instances: ' + str(instances_ids))
-                client.terminate_instances(InstanceIds=instances_ids)
-            else:
-                logging.info('Invalid action: ' + action)
-        elif confirmation == 'n':
-            logging.info('No instances to ' + action)
         else:
-            logging.info(f"Invalid input: {confirmation}")
-    
+            confirmation = input('Are you sure you want to ' + action + ' these instances? [y/n]: \n' + str(instances_ids))
+            if confirmation == 'y':
+                if action == 'start':
+                    logging.info('Starting instances: ' + str(instances_ids))
+                    client.start_instances(InstanceIds=instances_ids)
+                elif action == 'stop':
+                    logging.info('Stopping instances: ' + str(instances_ids))
+                    client.stop_instances(InstanceIds=instances_ids)
+                elif action == 'terminate':
+                    logging.info('Terminating instances: ' + str(instances_ids))
+                    client.terminate_instances(InstanceIds=instances_ids)
+                else:
+                    logging.info('Action ' + action + ' is invalid')
+                    logging.info('Invalid action: ' + action)
+            elif confirmation == 'n':
+                logging.info('No instances to ' + action)
+        
+            else:
+                logging.info(f"Invalid input: {confirmation}")
+                logging.info('Action ' + action + ' is invalid')
+
     except Exception as e:
         logging.error(f"Unable to perform action, {e}")
 
