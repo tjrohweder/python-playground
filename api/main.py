7cbc53bd9b2a1d7edf65f28da519f6fc88e123a4
tjrohweder <tjrohweder@gmail.com>
2025-06-18 14:18:59 -0300

    Flask API sample

---
+++ api/main.py
@@ -0,0 +1,12 @@
+from flask import Flask, jsonify
+import subprocess
+
+app = Flask(__name__)
+
+@app.route('/date', methods=['GET'])
+def get_date():
+    result = subprocess.check_output(['date']).decode('utf-8')
+    return jsonify({'date': result.strip()})
+
+if __name__ == '__main__':
+    app.run()
