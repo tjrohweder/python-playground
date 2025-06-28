3bbab4d1b9c287d4602f51cd888ac8c98d9317b7
tjrohweder <tjrohweder@gmail.com>
2025-06-27 23:54:47 -0300

    Adding a simple BMI calculator

---
+++ .exercises/bmi.py
@@ -0,0 +1,7 @@
+def bmi (x, y):
+    return x / (y ** 2)
+
+weight = float(input("Type your weight: "))
+height = float(input("Type your height: "))
+
+print(f"Your BMI is {bmi(weight, height):.2f}")
