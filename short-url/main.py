518177254ce79d189baef9642ddb587216ed0005
tjrohweder <tjrohweder@gmail.com>
2025-07-01 18:59:06 -0300

    Improving variable name

--- short-url/main.py
+++ short-url/main.py
@@ -1,6 +1,6 @@
 import pyshorteners
 
 long_url = "https://www.google.com/"
-tyni_url = pyshorteners.Shortener().tinyurl.short(long_url)
+short_url = pyshorteners.Shortener().tinyurl.short(long_url)
 
-print(tyni_url)
+print(short_url)
