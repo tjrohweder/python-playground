74826c5409f75571999c1417f827a520c5c30942
tjrohweder <tjrohweder@gmail.com>
2025-07-05 16:45:55 -0300

    Improving formating on code for printing messsages do the user

--- aws/ec2-start-stop/main.py
+++ aws/ec2-start-stop/main.py
@@ -41,28 +41,26 @@ def ec2_action(client, instances_ids, instance_status):
     try:
         action = sys.argv[1]
         if action == 'list':
-            logging.info('Instances available: ' + str(instances_ids) + ' - ' + str(instance_status))
+            logging.info(f'Instances available: {instances_ids} - {instance_status}')
 
         else:
-            confirmation = input('Are you sure you want to ' + action + ' these instances? [y/n]: \n' + str(instances_ids))
+            confirmation = input(f'Are you sure you want to {action} these instances? [y/n]: \n {instances_ids}')
             if confirmation == 'y':
                 if action == 'start':
-                    logging.info('Starting instances: ' + str(instances_ids))
+                    logging.info(f'Starting instances: {instances_ids}')
                     client.start_instances(InstanceIds=instances_ids)
                 elif action == 'stop':
-                    logging.info('Stopping instances: ' + str(instances_ids))
+                    logging.info(f'Stopping instances: {instances_ids}')
                     client.stop_instances(InstanceIds=instances_ids)
                 elif action == 'terminate':
-                    logging.info('Terminating instances: ' + str(instances_ids))
+                    logging.info(f'Terminating instances: {instances_ids}')
                     client.terminate_instances(InstanceIds=instances_ids)
                 else:
-                    logging.info('Action ' + action + ' is invalid')
-                    logging.info('Invalid action: ' + action)
+                    logging.info(f'Invalid action: {action}')
             elif confirmation == 'n':
-                logging.info('No instances to ' + action)
+                logging.info(f'No instances to {action}')
             else:
                 logging.info(f"Invalid input: {confirmation}")
-                logging.info('Action ' + action + ' is invalid')
 
     except Exception as e:
         logging.error(f"Unable to perform action, {e}")
