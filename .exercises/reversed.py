528777ad480fa7dad285c781c88ecb9bdb509af7
tjrohweder <tjrohweder@gmail.com>
2025-07-06 22:22:58 -0300

    Adding a reversed word exercise

---
+++ .exercises/reversed.py
@@ -0,0 +1,8 @@
+word = input('Type a word: ')
+
+reversed_word = word[::-1]
+
+if word == reversed_word:
+    print(f'{word} is the same as reversed')
+else:
+    print(f'{word} is not the same as reserved')
