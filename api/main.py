ea2c92d29433033e377fb9e28380810b139f43a7
tjrohweder <tjrohweder@gmail.com>
2025-07-28 17:06:33 -0300

    Adding database connection for mysql

--- api/main.py
+++ api/main.py
@@ -1,12 +1,19 @@
 from flask import Flask, jsonify
+import sqlalchemy as db
 import subprocess
 
+#App
 app = Flask(__name__)
+date = subprocess.check_output(['date']).decode('utf-8')
+
+#Database
+engine = db.create_engine("mysql+mysqlconnector://app_user:An0thrS3crt@db:3306/app_db")
+conn = engine.connect()
+metadata = db.MetaData()
 
 @app.route('/date', methods=['GET'])
 def get_date():
-    result = subprocess.check_output(['date']).decode('utf-8')
-    return jsonify({'date': result.strip()})
+    return jsonify({'date': date.strip()})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
